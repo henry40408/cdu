1557aa2d746414d72a075dff0a45984b480a0dbb
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-07-30 10:35:15 +0800

    refactor: wrap functions in a struct

--- src/main.rs
+++ src/main.rs
@@ -55,9 +55,10 @@ impl Opts {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::from_args();
+    let context = Arc::new(Cdu::new(opts));
 
     if env::var_os("RUST_LOG").is_none() {
-        if opts.debug {
+        if context.is_debug() {
             env::set_var("RUST_LOG", "cdu=debug");
         } else {
             env::set_var("RUST_LOG", "cdu=info");
@@ -66,17 +67,17 @@ async fn main() -> anyhow::Result<()> {
 
     pretty_env_logger::init();
 
-    if opts.daemon {
-        run_daemon(&opts).await?;
+    if context.is_daemon() {
+        run_daemon(context).await?;
     } else {
-        run_once(&opts).await?;
+        context.run().await?;
     }
 
     Ok(())
 }
 
-async fn run_daemon(opts: &Opts) -> anyhow::Result<()> {
-    let schedule = Schedule::from_str(&opts.cron)?;
+async fn run_daemon(context: Arc<Cdu>) -> anyhow::Result<()> {
+    let schedule = Schedule::from_str(context.cron())?;
 
     for datetime in schedule.upcoming(chrono::Utc) {
         info!("update DNS records at {}", datetime);
@@ -91,9 +92,10 @@ async fn run_daemon(opts: &Opts) -> anyhow::Result<()> {
 
         let strategy = ExponentialBackoff::from_millis(10).map(jitter).take(3);
         let instant = Instant::now();
+        let context = context.clone();
         tokio_retry::RetryIf::spawn(
             strategy,
-            || run_once(opts),
+            || context.run(),
             |e: &anyhow::Error| e.is::<ApiFailure>() || e.is::<PublicIPError>(),
         )
         .await?;
@@ -116,94 +118,116 @@ impl std::fmt::Display for PublicIPError {
 
 impl std::error::Error for PublicIPError {}
 
-async fn run_once(opts: &Opts) -> anyhow::Result<()> {
-    let ip_address = public_ip::addr_v4().await.ok_or(PublicIPError)?;
+struct Cdu {
+    opts: Opts,
+}
 
-    let credentials = Credentials::UserAuthToken {
-        token: opts.token.clone(),
-    };
-    let config = HttpApiClientConfig {
-        http_timeout: Duration::from_secs(HTTP_TIMEOUT),
-        ..Default::default()
-    };
-    let client = Arc::new(Client::new(credentials, config, Environment::Production)?);
+impl Cdu {
+    fn new(opts: Opts) -> Self {
+        Self { opts }
+    }
 
-    debug!("public IPv4 address: {}", &ip_address);
+    fn is_debug(&self) -> bool {
+        self.opts.debug
+    }
 
-    let params = ListZones {
-        params: ListZonesParams {
-            name: Some(opts.zone.clone()),
+    fn is_daemon(&self) -> bool {
+        self.opts.daemon
+    }
+
+    fn cron(&self) -> &str {
+        &self.opts.cron
+    }
+
+    async fn run(&self) -> anyhow::Result<()> {
+        let ip_address = public_ip::addr_v4().await.ok_or(PublicIPError)?;
+
+        let credentials = Credentials::UserAuthToken {
+            token: self.opts.token.clone(),
+        };
+        let config = HttpApiClientConfig {
+            http_timeout: Duration::from_secs(HTTP_TIMEOUT),
             ..Default::default()
-        },
-    };
-    let res: ApiSuccess<Vec<Zone>> = client.request(&params).await?;
-    let zone_id = match res.result.first() {
-        Some(zone) => zone.id.to_string(),
-        None => bail!("zone not found: {}", opts.zone),
-    };
+        };
+        let client = Arc::new(Client::new(credentials, config, Environment::Production)?);
 
-    debug!("zone found: {} ({})", &opts.zone, &zone_id);
+        debug!("public IPv4 address: {}", &ip_address);
 
-    let mut futs = vec![];
-    for record_name in opts.record_name_list() {
-        let client = client.clone();
-        let zone_id = zone_id.clone();
-        futs.push(tokio::spawn(async move {
-            let params = ListDnsRecords {
-                zone_identifier: &zone_id,
-                params: ListDnsRecordsParams {
-                    name: Some(record_name.clone()),
-                    ..Default::default()
-                },
-            };
-            let res: ApiSuccess<Vec<DnsRecord>> = client.request(&params).await?;
-            let dns_record = match res.result.first() {
-                Some(dns_record) => dns_record,
-                None => bail!("DNS record not found: {}", record_name),
-            };
-            debug!("DNS record found: {} ({})", &record_name, &dns_record.id);
-            Ok((dns_record.id.clone(), record_name))
-        }));
-    }
+        let params = ListZones {
+            params: ListZonesParams {
+                name: Some(self.opts.zone.clone()),
+                ..Default::default()
+            },
+        };
+        let res: ApiSuccess<Vec<Zone>> = client.request(&params).await?;
+        let zone_id = match res.result.first() {
+            Some(zone) => zone.id.to_string(),
+            None => bail!("zone not found: {}", self.opts.zone),
+        };
 
-    let mut dns_record_ids = vec![];
-    for fut in futures::future::join_all(futs).await {
-        let (dns_record_id, record_name) = fut??;
-        dns_record_ids.push((dns_record_id, record_name));
-    }
+        debug!("zone found: {} ({})", &self.opts.zone, &zone_id);
 
-    let mut futs: Vec<JoinHandle<anyhow::Result<(String, String, String)>>> = vec![];
-    for (dns_record_id, record_name) in dns_record_ids {
-        let client = client.clone();
-        let zone_id = zone_id.clone();
-        futs.push(tokio::spawn(async move {
-            let params = UpdateDnsRecord {
-                zone_identifier: &zone_id,
-                identifier: &dns_record_id,
-                params: UpdateDnsRecordParams {
-                    name: &record_name,
-                    content: DnsContent::A {
-                        content: ip_address,
+        let mut tasks = vec![];
+        for record_name in self.opts.record_name_list() {
+            let client = client.clone();
+            let zone_id = zone_id.clone();
+            tasks.push(tokio::spawn(async move {
+                let params = ListDnsRecords {
+                    zone_identifier: &zone_id,
+                    params: ListDnsRecordsParams {
+                        name: Some(record_name.clone()),
+                        ..Default::default()
                     },
-                    proxied: None,
-                    ttl: None,
-                },
-            };
-            let res: ApiSuccess<DnsRecord> = client.request(&params).await?;
-            let dns_record = res.result;
-            let content = match dns_record.content {
-                DnsContent::A { content } => content.to_string(),
-                _ => "(not an A record)".into(),
-            };
+                };
+                let res: ApiSuccess<Vec<DnsRecord>> = client.request(&params).await?;
+                let dns_record = match res.result.first() {
+                    Some(dns_record) => dns_record,
+                    None => bail!("DNS record not found: {}", record_name),
+                };
+                debug!("DNS record found: {} ({})", &record_name, &dns_record.id);
+                Ok((dns_record.id.clone(), record_name))
+            }));
+        }
 
-            Ok((record_name, dns_record_id, content))
-        }));
+        let mut dns_record_ids = vec![];
+        for task in futures::future::join_all(tasks).await {
+            let (dns_record_id, record_name) = task??;
+            dns_record_ids.push((dns_record_id, record_name));
+        }
+
+        let mut tasks: Vec<JoinHandle<anyhow::Result<(String, String, String)>>> = vec![];
+        for (dns_record_id, record_name) in dns_record_ids {
+            let client = client.clone();
+            let zone_id = zone_id.clone();
+            tasks.push(tokio::spawn(async move {
+                let params = UpdateDnsRecord {
+                    zone_identifier: &zone_id,
+                    identifier: &dns_record_id,
+                    params: UpdateDnsRecordParams {
+                        name: &record_name,
+                        content: DnsContent::A {
+                            content: ip_address,
+                        },
+                        proxied: None,
+                        ttl: None,
+                    },
+                };
+                let res: ApiSuccess<DnsRecord> = client.request(&params).await?;
+                let dns_record = res.result;
+                let content = match dns_record.content {
+                    DnsContent::A { content } => content.to_string(),
+                    _ => "(not an A record)".into(),
+                };
+
+                Ok((record_name, dns_record_id, content))
+            }));
+        }
+
+        for task in futures::future::join_all(tasks).await {
+            let (r, d, c) = task??;
+            debug!("DNS record updated: {} ({}) -> {}", &r, &d, &c);
+        }
+
+        Ok(())
     }
-
-    for fut in futures::future::join_all(futs).await {
-        let (r, d, c) = fut??;
-        debug!("DNS record updated: {} ({}) -> {}", &r, &d, &c);
-    }
-
-    Ok(())
 }
